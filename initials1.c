278308214fe5617d3ee6eb20109f0de183846047
Jonathan Demirgian <trydemirgian@gmail.com>
2017-10-11 15:36:45 +0000

    First commit on intials problem.

---
+++ initials1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/* 1. Get cl args
+* 2. For each cl arg (after the first script arg which is script name):
+*   A. Get first char of cl arg.
+*   B. Convert that char to uppercase if not already.
+*   C. Take that char print it to the screen.
+*/
+
+int main(int argc, string argv[])
+{
+
+}
