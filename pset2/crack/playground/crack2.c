656168ea021f848ffe861fc9a9ad7c9dae803345
Jonathan Demirgian <trydemirgian@gmail.com>
2017-10-13 18:27:24 +0000

    Multiple counters was stupid. Changed to array.
    I should have made my counter work in base 52, but I don't know enough
    math to do that easily.

---
+++ pset2/crack/playground/crack2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <unistd.h>
+#include <crypt.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Failure: More or less than 1 arg supplied.\n");
+        return 1;
+    }
+
+    string HASH = argv[1];
+
+    int charSet[52] = {
+        65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
+        76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87,
+        88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104,
+        105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
+        115, 116, 117, 118, 119, 120, 121, 122
+    };
+    char attempt[5] = {'\0', '\0', '\0', '\0', '\0'};
+
+    string result = "";
+
+    int counters[] = {0,0,0,0};
+
+    while (strcmp(HASH, result) != 0)
+    {
+        if (counters[0] < 52)
+        {
+            attempt[0] = charSet[counters[0]];
+            counters[0]++;
+        }
+        else if (counters[1] < 52)
+        {
+            counters[0] = 0;
+            attempt[1] = charSet[counters[1]];
+            counters[1]++;
+        }
+        else if (counters[2] < 52)
+        {
+            counters[0] = 0;
+            counters[1] = 0;
+            attempt[2] = charSet[counters[2]];
+            counters[2]++;
+        }
+        else if (counters[3] < 52)
+        {
+            counters[0] = 0;
+            counters[1] = 0;
+            counters[2] = 0;
+            attempt[3] = charSet[counters[3]];
+            counters[3]++;
+        }
+        result = crypt(attempt, HASH);
+        //printf("%s\n", result);
+        //printf("%i%i%i%i\n", counter0, counter1, counter2, counter3);
+    };
+    printf("pass: '%s', gives hash: %s\n", attempt, result);
+}
