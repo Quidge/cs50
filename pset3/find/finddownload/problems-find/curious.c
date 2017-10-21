64f2cc8fcdfccd05332b7b2fb6d473977f57416d
Jonathan Demirgian <trydemirgian@gmail.com>
2017-10-21 18:50:58 +0000

    Got the counting sort working. Finally.

--- pset3/find/finddownload/problems-find/curious.c
+++ pset3/find/finddownload/problems-find/curious.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+const int MAX = 65536;
+
 void sort(int values[], int n);
 
 int main(void) {
@@ -23,22 +25,43 @@ int main(void) {
 
 void sort(int values[], int n)
 {
-    int finalArray[n];
-    int countingArray[65536];
+    // setup countingArray and be sure that all elements are value 0
+    int countingArray[MAX];
+    for (int i = 0; i < MAX; i++)
+    {
+        countingArray[i] = 0;
+    }
 
+    // loop through elements in array arg and increment values at corresponding
+    // index positions in countingArray
     for (int i = 0; i < n; i++)
     {
         countingArray[values[i]] = countingArray[values[i]] + 1;
     }
-    int currentIndex = 0;
-    for (int j = 0; j < 65536; j++)
+
+    // init a counter to be used as the index, and a temp resulting array
+    int sortedArrayIndex = 0;
+    int tempResult[n];
+    // run through all the possible numbers ([0, MAX]), but stop if original number of
+    // arg array elements have been sorted
+    for (int countArrayIndex = 0; countArrayIndex < MAX && sortedArrayIndex < n; countArrayIndex++)
     {
-        for (int k = 0; countingArray[j] > 0 && k < countingArray[j]; k++)
+        // most of the time this will be 0, when it isn't
+        int currentCheck = countingArray[countArrayIndex];
+        if (currentCheck > 0)
         {
-            finalArray[currentIndex] = countingArray[j];
-            currentIndex++;
+            for (int k = 0; k < currentCheck; k++)
+            {
+                tempResult[sortedArrayIndex] = countArrayIndex;
+                sortedArrayIndex++;
+            }
         }
     }
-    values = finalArray;
+    // From how I understand find, I believe it wants sort to alter the the array. NOT create
+    // a copy ...which seems stupid.
+    for (int g = 0; g < n; g++)
+    {
+        values[g] = tempResult[g];
+    }
     return;
 }
