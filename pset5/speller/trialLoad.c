e9edc1e4d5cbd5eb2aec118422aa3acb91ac279d
Jonathan Demirgian <trydemirgian@gmail.com>
2017-12-03 00:59:43 +0000

    I think my insert, which is load() lite, works. Difficult to be sure because I can't simply print the structure like I could with an array. Seg faults when I try to run my size() implementation. Recursion is rough.

---
+++ pset5/speller/trialLoad.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Model heavily taken from CS50 shorts on tries and
+// http://www.crazyforcode.com/trie-data-structure-implementation/
+
+// Also, helpful understanding of when typedef is necessary from
+// https://stackoverflow.com/questions/612328/difference-between-struct-and-typedef-struct-in-c
+typedef struct node
+{
+    int end_string;
+    struct node* next_char[26];
+    struct node* prev_char;
+}
+node;
+
+unsigned int size(node *root);
+void insert(char *word, node *root);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Program supplied with more or less than 2 arguments\n");
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL)
+    {
+        printf("Could not locate %s\n", argv[1]);
+        return 2;
+    }
+    fclose(fp);
+
+    node *root = malloc(sizeof(node));
+
+    char *word = "he";
+    insert(word, root);
+    //unsigned int count = size(root);
+
+    printf("%i\n", size(root));
+
+    free(root);
+}
+
+void insert(char *word, node *root)
+{
+    node *head = root;
+    int c = 0;
+
+    while (word[c] != '\0')
+    {
+        if (head -> next_char[word[c] - 65] == NULL)
+        {
+            node *child = malloc(sizeof(node));
+            child -> prev_char = head;
+            child -> end_string = 0;
+
+            head -> next_char[word[c] - 65] = child;
+            head = child;
+        }
+        else
+        {
+            head = head -> next_char[word[c] - 65];
+        }
+        c++;
+    }
+    head -> end_string = 1;
+    printf("%i\n", c);
+}
+
+unsigned int size(node *root)
+{
+    unsigned int count = 0;
+    node *head = root;
+
+    if (head -> end_string == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (head -> next_char[i] != NULL)
+            {
+                count += size(head -> next_char[i]);
+            }
+        }
+    }
+
+    return count;
+}
