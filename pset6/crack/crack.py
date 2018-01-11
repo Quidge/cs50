766d063756f6dbfcb94d82699179b530dc8db1f7
Jonathan Demirgian <trydemirgian@gmail.com>
2018-01-11 20:38:30 +0000

    Finished it. Found crack2 does it in a way that basically replicates the C version, while crack.3 uses some stuff I learned about list comprehension. List comp rabbit hole came from a SO answer, where someone eseems to be asking exactly what I was interested in. Go figure. This page helpeded too: http://www.dalkescientific.com/writings/NBN/list_comps.html

--- pset6/crack/crack.py
+++ pset6/crack/crack.py
@@ -9,17 +9,35 @@ def main():
 
     chSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     chSet2 = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    chSet3 = "ROFL"
+    chSet3 = "rofl "
 
-    for ch0 in chSet3:
-        for ch1 in chSet3:
-            for ch2 in chSet3:
-                for ch3 in chSet3:
-                    for ch4 in chSet3:
-                        string = f"{ch0}{ch1}{ch2}{ch3}{ch4}"
+    '''for ch0 in chSet:
+        for ch1 in chSet2:
+            for ch2 in chSet2:
+                for ch3 in chSet2:
+                        string = f"{ch0}{ch1}{ch2}{ch3}"
+                        #print(string)
+                        print(crypt(string, argv[1]))
                         if crypt(string, argv[1]) == argv[1]:
                             print("what")
                             return 5
+    '''
+    print(crack(argv[1], chSet))
+
+def crack(hashval, chSet):
+    #withspace = " " + chSet
+    chSet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+    for ch0 in chSet:
+        for ch1 in chSet:
+            for ch2 in chSet:
+                for ch3 in chSet:
+                    for ch4 in chSet:
+                        #attempt = f"{ch0}{ch1}{ch2}{ch3}{ch4}".rstrip()
+                        attempt = f"{ch4}{ch3}{ch2}{ch1}"
+                        if crypt(attempt, argv[1]) == argv[1]:
+                            return attempt
 
 if __name__ == "__main__":
     main()
