45b4c6824c32ce48e5799afd3668e40e071d4459
Jonathan Demirgian <trydemirgian@gmail.com>
2018-01-12 15:00:06 +0000

    I did a presentation on Tries a few nights ago and someone asked the real questions. I didn't have any answers about how much memory a trie takes up, so I've built in some stuff to count the number of nodes. I'd like to run this whole set again with some different structs and compare the memory/time differences.

---
+++ pset5/spellerALT/dictionary.h
@@ -0,0 +1,65 @@
+/**
+ * Declares a dictionary's functionality.
+ */
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+// maximum length for a word
+// (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
+#define LENGTH 45
+
+// maximum size of character set array (26 alpha chars + 1 for
+// apostrophe)
+#define CHARSETSIZE 27
+
+// this dictionary will be built using trie structures. below
+// is the prototype for that struct.
+typedef struct node
+{
+    int end_string;
+    struct node* next_char[CHARSETSIZE];
+    struct node* prev_char;
+}
+node;
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char *word);
+
+/**
+ * Inserts individual words into root node.
+ */
+int insert(char word[]);
+
+/**
+ * Loads dictionary into memory. Returns true if successful else false.
+ */
+bool load(const char *dictionary);
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void);
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void);
+
+/**
+ * Can crawl through a trie struct and print all words found.
+ */
+//void printWords(node *head, char *prefix, int wordLen);
+
+/**
+ * Returns the number of child nodes for a given node. Designed to be used recursively.
+ */
+unsigned int count_num_nodes();
+
+
+#endif // DICTIONARY_H
